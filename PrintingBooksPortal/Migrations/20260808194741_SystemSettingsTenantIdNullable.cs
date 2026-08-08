284ffecab3c34313fee97e63db7a7b0ead14719b
mustfamaheer-dotcom <mustfamaheer-dotcom@users.noreply.github.com>
2026-08-08 22:48:35 +0300

    Add migration: make SystemSettings.TenantId nullable (global-scope rows)

---
+++ PrintingBooksPortal/Migrations/20260808194741_SystemSettingsTenantIdNullable.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PrintingBooksPortal.Migrations
+{
+    /// <inheritdoc />
+    public partial class SystemSettingsTenantIdNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Global (SystemAdmin) settings rows use TenantId = NULL; make the column nullable.
+            migrationBuilder.AlterColumn<int>(
+                name: "TenantId",
+                table: "SystemSettings",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "TenantId",
+                table: "SystemSettings",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
